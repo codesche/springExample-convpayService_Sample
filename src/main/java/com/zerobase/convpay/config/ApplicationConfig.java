8494ceba5a2fd81b79ae74e4d7cce62aa536eca8
minsungha <hms7709@naver.com>
2022-10-18 15:33:45 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.zerobase.convpay.config;
+
+import com.zerobase.convpay.ConvpayApplication;
+import com.zerobase.convpay.service.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Configuration
+@ComponentScan(basePackageClasses = ConvpayApplication.class)
+public class ApplicationConfig {
+
+
+}
